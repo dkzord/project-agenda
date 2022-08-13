6ae0c491eb13f1c6c76c179c8911fbbef1afc931
Fernando Cavalcanti <comododk@gmail.com>
2022-08-13 13:04:13 -0300

    Feat: create contato

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -20,3 +20,13 @@ exports.csrfMiddleware = (req, resp, next) => {
   resp.locals.csrfToken = req.csrfToken();
   next();
 }
+
+exports.loginRequired = (req, res, next) => {
+  if (!req.session.user) {
+    req.flash('errors', 'Você precisa fazer login!');
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+
+  next();
+}
